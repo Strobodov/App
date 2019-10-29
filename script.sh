8913daa7564d70854ea51135917e3e364857f979
Marcel Kornegoor <mkornegoor@localhost.localdomain>
2019-10-29 21:07:17 +0100

    minor bug fixes and improved comments

--- script.sh
+++ script.sh
@@ -23,30 +23,33 @@ az group create -n $group_name --location $az_location
 wait
 echo -e "\e[34mResource Group ready\e[0m"
 
-#deploy VM in azure. Make sure you have filled out all key value paires in config/variables
+#deploy VM in Azure. Make sure you have filled out all key value paires in config/variables
 az vm create --name $vm_name --resource-group $group_name --admin-username $admin --generate-ssh-keys --image ubuntults --verbose --public-ip-address-dns-name $dns_name --size Standard_D2_v3
 wait
 echo -e "\e[34mVirtual Server ready\e[0m"
 
-#publish port 80 to the outside world (to make sure your webapp can be reached)
+#publish port 80 to the outside world (to make sure your webapp can be reached via http)
 az vm open-port --resource-group $group_name --name $vm_name --port 80
+wait
 
 #log into the Ubuntu VM in Azure
 ssh -i $ssh_key -l "$admin" "$fqdn" -o StrictHostKeyChecking=no <<EOF
 
 #update the server (this may take a while)
-#sudo apt update && sudo apt upgrade --assume-yes --ignore-missing
-#wait
+sudo apt update && sudo apt upgrade --assume-yes --ignore-missing
+wait
 
-#install Docker
+#install docker
 sudo apt-get install docker.io --assume-yes
 wait
 
-#make a new folder for the Git repo
+#make a new folder for the git repo
 mkdir $path
+
+#change directory to the new folder
 cd $path
 
-#clone the git repo from github
+#clone the webapp git repo from github
 git clone https://github.com/Strobodov/App.git
 wait
 
