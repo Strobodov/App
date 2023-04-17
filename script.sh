3c017ddb81b7b0d3ed18ce83dd91e83ecc2f63bf
Marcel Kornegoor <54234375+Strobodov@users.noreply.github.com>
2023-04-17 21:55:28 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,48 +16,35 @@ source config/cred
 
 #login to Azure using AZ CLI
 az login -u $azure -p $azure_password
-wait
 
 #define name of the resource group and Azure region you want to deploy in. (see Azure documentation for current list of options)
 az group create -n $group_name --location $az_location
-wait
-echo -e "\e[34mResource Group ready\e[0m"
 
 #deploy VM in Azure. Make sure you have filled out all key value paires in config/variables
 az vm create --name $vm_name --resource-group $group_name --admin-username $admin --generate-ssh-keys --image ubuntults --verbose --public-ip-address-dns-name $dns_name --size Standard_D2_v3
-wait
-echo -e "\e[34mVirtual Server ready\e[0m"
 
 #publish port 80 to the outside world (to make sure your webapp can be reached via http)
 az vm open-port --resource-group $group_name --name $vm_name --port 80
-wait
 
 #log into the Ubuntu VM in Azure
-ssh -i $ssh_key -l "$admin" "$fqdn" -o StrictHostKeyChecking=no <<EOF
+ssh "$admin"@"$fqdn" -o StrictHostKeyChecking=no -o IdentitiesOnly=yes <<EOF
 
-#update the server (this may take a while)
-sudo apt update && sudo apt upgrade --assume-yes --ignore-missing
-wait
+#update package list
+sudo apt update
 
 #install docker
 sudo apt-get install docker.io --assume-yes
-wait
-
-#make a new folder for the git repo
-mkdir $path
-
-#change directory to the new folder
-cd $path
 
 #clone the webapp git repo from github
 git clone https://github.com/Strobodov/App.git
-wait
+
+#change directory
+cd App
 
 #build the docker container
-sudo docker build $path/App/docker --tag=mywebapp
-wait
+sudo docker build docker --tag=mywebapp
 
 #run the container
 sudo docker run -d -p 80:80 mywebapp
 EOF
-echo -e "\e[34mYour @AT Computing Webapp is ready\e[0m"
+printf "\rOpen your browser and open %s\n" "$fqdn"
